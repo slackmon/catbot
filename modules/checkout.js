92185629e4fe810b6da90b8ced5f963c46dbf47d
Monica Wilkinson <mwilkinson@slack-corp.com>
2016-12-09 08:50:27 -0800

    Read the links and get topics out

--- modules/checkout.js
+++ modules/checkout.js
@@ -1,29 +1,47 @@
+var textScraper = require('text-scraper');
+// TODO: Compute tag cloud from channel or interests
+var globalTopics = ['Ruby', 'PHP', 'Slack', 'NodeJS', 'engineer', 'Salesforce', 'Workday', 'analytics', 'enterprise'];
+
 exports.handle = function (sender, pieces, storageFactory, callback) {
-    var link = pieces.shift();
+    var m = pieces.match(/<(http.+)>/gi) || [];
+    for(var i=1; i < m.length; i++) {
+        var link = m[i];
+        console.log('Found link: ' + link);
+        var linkContent;
+        textScraper.scrapeURL(link, function(err, res) {
+            if (err) {
+                console.log('Error reading link: ' + link + ' is ' + err);
+            } else {
+                console.log('Content for link: ' + link + ' is:\n ' + res);
+                linkContent = res;
+                var topicCloud = {};
+                var topics = [];
 
-    // skip 'requires' if first word of endorsement
-    if (pieces[0] == 'requires') {
-        pieces.shift();
-    }
+                for (var j =0; j < globalTopics.length; j++){
+                    var topic = globalTopics[j];
+                    var reg = RegExp(topic, 'gi');
+                    var m2 = linkContent.match(reg) || [];
+                    if (m2.length>0){
+                        topics.push(topic + ' :' +  topic.toLowerCase() +':');
+                        topics[topic.toLowerCase()] = m2.length;
+                    }
+                }
 
-    var skill = pieces.join(' ');
+                if (topics.length > 0) {
+                    var linkStorage = storageFactory.getGlobalStorage(link);
+                    linkStorage.setItem('contains', JSON.stringify(topicCloud));
 
-    console.log('Link ' + link + ' requires ' + skill);
+                    callback({
+                        'message': ':link: ' + link + ' covers topics: ' + topics.join(', ')
+                    });
+                } else {
+                    callback({
+                        'message': ':disappointed: ugh Sorry I could not find any relevant content for link: ' + link
+                    });
+                }
+            }
 
-    var linkStorage = storageFactory.getGlobalStorage(link);
-    linkStorage.getItem('requires', function(skills){
-        skills = JSON.parse(skills || '{}');
-
-        if (skills[skills]) {
-            skills[skills] += 1;
-        } else {
-            skills[skill] = 1;
-        }
-
-        linkStorage.setItem('requires', JSON.stringify(skills));
-
-        callback({
-            'message': 'Got it. Link: ' + link + ' requires ' + skill
         });
-    });
+
+    }
 };

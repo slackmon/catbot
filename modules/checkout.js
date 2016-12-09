033a8431d8ee9c532995b31af4d16475c42994b7
Monica Wilkinson <mwilkinson@slack-corp.com>
2016-12-09 06:05:47 -0800

    test

--- modules/checkout.js
+++ modules/checkout.js
@@ -1,29 +1,30 @@
 exports.handle = function (sender, pieces, storageFactory, callback) {
-    var link = pieces.shift();
-
-    // skip 'requires' if first word of endorsement
-    if (pieces[0] == 'requires') {
-        pieces.shift();
-    }
-
-    var skill = pieces.join(' ');
-
-    console.log('Link ' + link + ' requires ' + skill);
-
-    var linkStorage = storageFactory.getGlobalStorage(link);
-    linkStorage.getItem('requires', function(skills){
-        skills = JSON.parse(skills || '{}');
-
-        if (skills[skills]) {
-            skills[skills] += 1;
-        } else {
-            skills[skill] = 1;
-        }
-
-        linkStorage.setItem('requires', JSON.stringify(skills));
-
-        callback({
-            'message': 'Got it. Link: ' + link + ' requires ' + skill
-        });
-    });
-}
+    // var link = pieces.shift();
+    //
+    // // skip 'requires' if first word of endorsement
+    // if (pieces[0] == 'requires') {
+    //     pieces.shift();
+    // }
+    //
+    // var skill = pieces.join(' ');
+    //
+    // console.log('Link ' + link + ' requires ' + skill);
+    //
+    // var linkStorage = storageFactory.getGlobalStorage(link);
+    // linkStorage.getItem('requires', function(skills){
+    //     skills = JSON.parse(skills || '{}');
+    //
+    //     if (skills[skills]) {
+    //         skills[skills] += 1;
+    //     } else {
+    //         skills[skill] = 1;
+    //     }
+    //
+    //     linkStorage.setItem('requires', JSON.stringify(skills));
+    //
+    //     callback({
+    //         'message': 'Got it. Link: ' + link + ' requires ' + skill
+    //     });
+    // });
+    callback({"message": "Hi"});
+};
--- modules/loves.js
+++ modules/loves.js
@@ -1,24 +1,25 @@
 exports.handle = function (sender, pieces, storageFactory, callback) {
-    var user = pieces.shift();
-
-    var thing = pieces.join(' ');
-
-    console.log(user + ' loves ' + loved);
-
-    var userStorage = storageFactory.getUserStorage(user);
-    userStorage.getItem('loves', function(loves){
-        loves = JSON.parse(loves || '{}');
-
-        if (loves[thing]) {
-            loves[thing] += 1;
-        } else {
-            loves[thing] = 1;
-        }
-
-        userStorage.setItem('loves', JSON.stringify(loves));
-
-        callback({
-            'message': user + ' loves ' + thing
-        });
-    });
-}
+    // var user = pieces.shift();
+    //
+    // var thing = pieces.join(' ');
+    //
+    // console.log(user + ' loves ' + loved);
+    //
+    // var userStorage = storageFactory.getUserStorage(user);
+    // userStorage.getItem('loves', function(loves){
+    //     loves = JSON.parse(loves || '{}');
+    //
+    //     if (loves[thing]) {
+    //         loves[thing] += 1;
+    //     } else {
+    //         loves[thing] = 1;
+    //     }
+    //
+    //     userStorage.setItem('loves', JSON.stringify(loves));
+    //
+    //     callback({
+    //         'message': user + ' loves ' + thing
+    //     });
+    // });
+    callback({"message": "Hi again"});
+};
